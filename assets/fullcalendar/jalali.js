4b3044a5b3f85349c61a8adb1c8f54a0525c786f
Mohammad Ebrahim <faravaghi@yahoo.com>
2013-10-17 16:53:26 +0330

    Fix Bug in Jalali.js

--- assets/fullcalendar/jalali.js
+++ assets/fullcalendar/jalali.js
@@ -127,7 +127,7 @@ Date.prototype.setJalaliMonth = function(m, d) {
 	var gy = this.getFullYear();
 	var j = JalaliDate.gregorianToJalali(gy, gm+1, gd);
 	if (m > 11) {
-		j[0] += math.floor(m / 12);
+		j[0] += Math.floor(m / 12);
 		m = m % 12;
 	}
 	j[1] = m+1;
